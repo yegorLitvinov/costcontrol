fb168f3c67fce311a13f6717cd16bb5761011ad1
Egor Litvinov <yegor.litvinov@yandex.ru>
2018-11-01 02:40:39 +0300

    Fix rsync

--- ansible_drone_plugin/entrypoint.sh
+++ ansible_drone_plugin/entrypoint.sh
@@ -4,5 +4,4 @@ echo "$ID_RSA" > ~/.ssh/id_rsa
 echo "$ID_RSA_PUB" > ~/.ssh/id_rsa.pub
 chmod 0600 ~/.ssh/*
 echo '195.201.27.44 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBNSLwIS38fXmWekn7JAymlJ2zXCN8cDnVBVsyHHAV40RuQ/wQMLwZCtoGSKXPsGkK2F60J0VG35F7C2gM9cSgQ=' >> ~/.ssh/known_hosts
-
 cd deploy && ansible-playbook deploy.yml
