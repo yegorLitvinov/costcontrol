93388688349def1871cdf76c3aa5275a37c6b42b
Egor Litvinov <yegor.litvinov@yandex.ru>
2018-11-01 01:31:00 +0300

    fix entrypoint.sh

--- ansible_drone_plugin/entrypoint.sh
+++ ansible_drone_plugin/entrypoint.sh
@@ -1,15 +1,15 @@
 mkdir -p ~/.ssh
 chmod 0700 ~/.ssh
-printf "$ID_RSA" > ~/.ssh/id_rsa
-printf "$ID_RSA_PUB" > ~/.ssh/id_rsa.pub
-chmod 0600 -R ~/.ssh/*
+echo "$ID_RSA" > ~/.ssh/id_rsa
+echo "$ID_RSA_PUB" > ~/.ssh/id_rsa.pub
+chmod 0600 ~/.ssh/*
 echo '195.201.27.44 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBNSLwIS38fXmWekn7JAymlJ2zXCN8cDnVBVsyHHAV40RuQ/wQMLwZCtoGSKXPsGkK2F60J0VG35F7C2gM9cSgQ=' >> ~/.ssh/known_hosts
 
 #
 ls -lah ~/.ssh
 cat ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa
 #
 
-cd deploy
-bash setup.sh
-ansible-playbook deploy.yml
+cd deploy && bash setup.sh
+cd deploy && ansible-playbook deploy.yml
